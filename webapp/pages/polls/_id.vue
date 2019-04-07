53d3b58d9bfbd197fb9353b3e41eaf8eb88b652e
ChrisDillinger <dillingerdevelopment@gmail.com>
2019-04-07 10:58:34 -0400

    Added page for single poll view with path param accessible via $route.params.id

---
+++ webapp/pages/polls/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a-row>
+      <a-col
+        :span="24"
+      >Sup brotha, you are looking a dynamically render page with id of: {{ $route.params.id }}</a-col>
+    </a-row>
+  </div>
+</template>
+<script>
+export default {
+  components: {}
+}
+</script>
+<style></style>
